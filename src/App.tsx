747ca4e73e69cebc1eefffe0555e4c0b3e01d480
rodrigo toledo <rodrit182@hotmail.com>
2021-09-13 21:05:43 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import 'bootswatch/dist/vapor/bootstrap.min.css'
 
-function App() {
+type FormElement = React.FormEvent<HTMLFormElement>;
+interface ITask {
+  name: string;
+  done: boolean;
+}
+
+
+function App(): JSX.Element {
+  const [task, setTask] = useState<string>('');
+  const [tasklist, setTaskList] = useState<ITask[]>([])
+  function handleSumit(event: FormElement) {
+    event.preventDefault();
+    addTask(task);
+    setTask('')
+  }
+  const addTask = (name: string,done:boolean) => {
+    const newTasks: ITask[] = [...tasklist, { name, done: false }]
+    setTaskList(newTasks)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container p-4">
+      <div className="row">
+        <div className="card">
+          <div className="col-md-6 offset-md-3">
+            <div className="card-body">
+              <form onSubmit={(event) => handleSumit(event)}>
+                <input autoFocus className="form-control" type="text" onChange={event => setTask(event.target.value)} value={task} />
+                <button className="btn btn-success btn-block mt-2">
+                  Save
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+        {
+          tasklist.map((task: ITask, i: number) => {
+            return (
+              <div className="card card-body mt-2" key={i}>
+                <h2>{task.name}</h2>
+                <h2>{task.done}</h2>
+              </div>
+              )
+
+          })
+        }
+      </div>
     </div>
   );
 }
